e48c104a9fbf6a5447f9ec1c817b16b465349c31
AdamTheisen <atheisen@anl.gov>
2023-03-12 14:03:37 +0000

    ENH: met plots

--- dqr_training/dqr_ml.py
+++ dqr_training/dqr_ml.py
@@ -18,8 +18,10 @@ import glob
 from datetime import datetime
 import random
 
-ds='sgp30ebbrE13.b1'
-varname='latent_heat_flux' # sensible_heat_flux net_radiation 
+#ds='sgp30ebbrE13.b1'
+#varname='sensible_heat_flux' # sensible_heat_flux net_radiation 
+ds = 'sgpmetE13.b1'
+varname = 'temp_mean'
 vis_sdate='20220101'
 vis_edate='20221231'
 min_max=0
